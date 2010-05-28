52533ecf370f914e309cb9f9a13f98f8f6bbb5cc
Adam Vandenberg <flangy@gmail.com>
2010-05-28 10:47:33 -0700

    Comment for test_issue.

--- tests/unit.py
+++ tests/unit.py
@@ -8,5 +8,6 @@ class ReprTests(unittest.TestCase):
     """__repr__ must return strings, not unicode objects."""
 
     def test_issue(self):
+        """Issues can have non-ASCII characters in the title."""
         i = Issue(title = u'abcdé')
         self.assertEqual(str, type(repr(i)))
