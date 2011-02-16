de7638f8e69d78bbec004cd1e22696d3d32aabc1
James Rowe <jnrowe@gmail.com>
2011-02-16 14:33:29 +0000

    Fix issue label addition/removal.
    Now need to force POST, really should have been doing it from the outset.

--- github2/issues.py
+++ github2/issues.py
@@ -83,11 +83,11 @@ class Issues(GithubCommand):
 
     def add_label(self, project, number, label):
         return self.make_request("label/add", project, label, str(number),
-                                 filter="labels")
+                                 filter="labels", method="POST")
 
     def remove_label(self, project, number, label):
         return self.make_request("label/remove", project, label, str(number),
-                                 filter="labels")
+                                 filter="labels", method="POST")
 
     def comment(self, project, number, comment):
         """Comment on an issue."""
