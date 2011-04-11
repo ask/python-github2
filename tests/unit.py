f4731307b5e700c94ff188514fb171c666d933e7
James Rowe <jnrowe@gmail.com>
2011-04-11 15:50:37 +0100

    Added httplib2.Http mock for use in tests.

--- tests/unit.py
+++ tests/unit.py
@@ -1,8 +1,38 @@
 # -*- coding: latin-1 -*-
+import os
 import unittest
+
+from email import message_from_file
+
+import httplib2
 
 from github2.issues import Issue
 from github2.client import Github
+
+
+HTTP_DATA_DIR = "tests/data/"
+
+
+class HttpMock(object):
+    """Simple Http mock that returns saved entries
+
+    Implementation tests should never span network boundaries
+    """
+
+    def __init__(self, cache=None, timeout=None, proxy_info=None):
+        pass
+
+    def request(self, uri, method='GET', body=None, headers=None,
+                redirections=5, connection_type=None):
+        file = os.path.join(HTTP_DATA_DIR, httplib2.safename(uri))
+        print file
+        if os.path.exists(file):
+            response = message_from_file(open(file))
+            body = response.get_payload()
+            headers = httplib2.Response(response)
+            return (headers, body)
+        else:
+            return (httplib2.Response({"status": "404"}), "")
 
 
 class ReprTests(unittest.TestCase):
