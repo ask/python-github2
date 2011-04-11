06bb27d5057fdfcd97a1c52f923f39deabfbc8b8
James Rowe <jnrowe@gmail.com>
2011-04-11 20:44:14 +0100

    More documentation for Issues class' methods.

--- github2/issues.py
+++ github2/issues.py
@@ -40,84 +40,127 @@ class Issues(GithubCommand):
     def search(self, project, term, state="open"):
         """Get all issues for project that match term with given state.
 
-        ``project`` is a string with the project owner username and repository
-        name separated by ``/`` (e.g. ``ask/pygithub2``).
-        ``term`` is a string to search issues for.
-        ``state`` can be either ``open`` or ``closed``.
+        :param str project: GitHub project
+        :param str term: term to search issues for
+        :param str state: can be either ``open`` or ``closed``.
         """
         return self.get_values("search", project, state,
                                urllib.quote_plus(term), filter="issues",
                                datatype=Issue)
 
     def list(self, project, state="open"):
-        """Get all issues for project' with state'.
+        """Get all issues for project with given state.
 
-        ``project`` is a string with the project owner username and repository
-        name separated by ``/`` (e.g. ``ask/pygithub2``).
-        ``state`` can be either ``open`` or ``closed``.
+        :param str project: GitHub project
+        :param str state: can be either ``open`` or ``closed``.
         """
         return self.get_values("list", project, state, filter="issues",
                                datatype=Issue)
 
     def list_by_label(self, project, label):
-        """Get all issues for project' with label'.
+        """Get all issues for project with label.
 
-        ``project`` is a string with the project owner username and repository
-        name separated by ``/`` (e.g. ``ask/pygithub2``).
-        ``label`` is a string representing a label (e.g., ``bug``).
+        :param str project: GitHub project
+        :param str label:  a string representing a label (e.g., ``bug``).
         """
         return self.get_values("list", project, "label", label, filter="issues",
                                datatype=Issue)
 
     def list_labels(self, project):
-        """Get all labels for project'.
+        """Get all labels for project.
 
-        ``project`` is a string with the project owner username and repository
-        name separated by ``/`` (e.g. ``ask/pygithub2``).
+        :param str project: GitHub project
         """
         return self.get_values("labels", project, filter="labels")
 
     def show(self, project, number):
-        """Get all the data for issue by issue-number."""
+        """Get all the data for issue by issue-number.
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        """
         return self.get_value("show", project, str(number),
                               filter="issue", datatype=Issue)
 
     def open(self, project, title, body):
-        """Open up a new issue."""
+        """Open up a new issue.
+
+        :param str project: GitHub project
+        :param str title: title for issue
+        :param str body: body for issue
+        """
         issue_data = {"title": title, "body": body}
         return self.get_value("open", project, post_data=issue_data,
                               filter="issue", datatype=Issue)
 
     def close(self, project, number):
+        """Close an issue
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        """
         return self.get_value("close", project, str(number), filter="issue",
                               datatype=Issue, method="POST")
 
     def reopen(self, project, number):
+        """Reopen a closed issue
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        """
         return self.get_value("reopen", project, str(number), filter="issue",
                               datatype=Issue, method="POST")
 
     def edit(self, project, number, title, body):
+        """Edit an existing issue
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        :param str title: title for issue
+        :param str body: body for issue
+        """
         issue_data = {"title": title, "body": body}
         return self.get_value("edit", project, str(number),
                               post_data=issue_data, filter="issue",
                               datatype=Issue)
 
     def add_label(self, project, number, label):
+        """Add a label to an issue
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        :param str label: label to attach to issue
+        """
         return self.make_request("label/add", project, label, str(number),
                                  filter="labels", method="POST")
 
     def remove_label(self, project, number, label):
+        """Remove an existing label from an issue
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        :param str label: label to remove from issue
+        """
         return self.make_request("label/remove", project, label, str(number),
                                  filter="labels", method="POST")
 
     def comment(self, project, number, comment):
-        """Comment on an issue."""
+        """Comment on an issue.
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        :param str comment: comment to attach to issue
+        """
         comment_data = {'comment': comment}
         return self.make_request("comment", project, str(number),
                                  post_data=comment_data,
                                  filter='comment')
 
     def comments(self, project, number):
-        """View comments on an issue."""
+        """View comments on an issue.
+
+        :param str project: GitHub project
+        :param int number: issue number in the Github database
+        """
         return self.get_values("comments", project, str(number),
                                filter="comments", datatype=Comment)
