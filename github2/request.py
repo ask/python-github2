590a50e291e66f2048bd672248cd74efc5d7dd75
Morgan Goose <morgan.goose@gmail.com>
2011-05-05 16:17:42 -0400

    fixed spelling error. more.

--- github2/request.py
+++ github2/request.py
@@ -3,7 +3,7 @@ import sys
 import time
 import httplib2
 import json
-from urllib.parse import (urlsplit, urlunsplit, parse_qa,
+from urllib.parse import (urlsplit, urlunsplit, parse_qs,
         urlencode, quote)
 
 
