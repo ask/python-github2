b92761a32fddc693c34bde4d59c0ea9a9722d98e
James Rowe <jnrowe@gmail.com>
2011-05-10 13:42:44 +0100

    Documented orgs/teams for use in Sphinx.

--- github2/organizations.py
+++ github2/organizations.py
@@ -33,36 +33,43 @@ class Organizations(GithubCommand):
     domain = "organizations"
 
     def show(self, organization):
+        """Get information on organization
+
+        :param str organization: organization to show
+        """
         return self.get_value(organization, filter="organization",
                               datatype=Organization)
 
     def list(self):
-        """Return a list of all of your organizations.
-        """
+        """Get list of all of your organizations"""
         return self.get_values('', filter="organizations",
                                datatype=Organization)
 
     def repositories(self):
-        """Return a list of all repositories in organizations you are
-        a member of.
-        """
+        """Get list of all repositories in organizations you are member of"""
         return self.get_values('repositories', filter="repositories",
                                datatype=Repository)
 
     def public_repositories(self, organization):
-        """Return a list of public repositories in an organization.
+        """Get list of public repositories in an organization
+
+        :param str organization: organization to list repositories for
         """
         return self.get_values(organization, 'public_repositories',
                                filter="repositories", datatype=Repository)
 
     def public_members(self, organization):
-        """Return a list of public members in an organization.
+        """Get list of public members in an organization
+
+        :param str organization: organization to list members for
         """
         return self.get_values(organization, 'public_members',
                                filter="users", datatype=User)
 
     def teams(self, organization):
-        """Return a list of teams in an organization.
+        """Get list of teams in an organization
+
+        :param str organization: organization to list teams for
         """
         return self.get_values(organization, 'teams',
                                filter="teams", datatype=Team)
--- github2/teams.py
+++ github2/teams.py
@@ -14,21 +14,34 @@ class Teams(GithubCommand):
     domain = "teams"
 
     def show(self, team_id):
+        """Get information on team_id
+
+        :param int team_id: team to get information for
+        """
         return self.get_value(str(team_id), filter="team", datatype=Team)
 
     def members(self, team_id):
-        """Return a list of all team members.
+        """Get list of all team members
+
+        :param int team_id: team to get information for
         """
         return self.get_values(str(team_id), "members", filter="users",
                                datatype=User)
 
     def repositories(self, team_id):
-        """Return a list of all team members.
+        """Get list of all team members
+
+        :param int team_id: team to get information for
         """
         return self.get_values(str(team_id), "repositories",
                                filter="repositories", datatype=Repository)
 
     def add_repository(self, team_id, repository):
+        """Add a repository to a team
+
+        :param int team_id: team to add repository to
+        :param str repository: GitHub project
+        """
         if isinstance(repository, Repository):
             repository = repository.project
         return self.make_request(str(team_id), "repositories", method="POST",
@@ -36,6 +49,11 @@ class Teams(GithubCommand):
                                  filter="repositories", datatype=Repository)
 
     def remove_repository(self, team_id, repository):
+        """Remove a repository to a team
+
+        :param int team_id: team to remove project from
+        :param str repository: GitHub project
+        """
         if isinstance(repository, Repository):
             repository = repository.project
         return self.make_request(str(team_id), "repositories", method="DELETE",
