6b20199a13838434273de26b8eaf454870fb6804
James Rowe <jnrowe@gmail.com>
2011-05-27 05:08:03 +0100

    Display links to source in documentation.
    Because Raymond Hettinger is absolutely on the money with
    http://rhettinger.wordpress.com/2011/01/28/open-your-source-more/ :)

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.%s" % ext for ext in ["autodoc", "todo", "intersphinx"]] + \
+extensions = ["sphinx.ext.%s" % ext
+              for ext in ["autodoc", "todo", "intersphinx", "viewcode"]] + \
     ["sphinxcontrib.%s" % ext for ext in ["cheeseshop", ]]
 
 # Add any paths that contain templates here, relative to this directory.
