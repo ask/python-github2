886ccc9ce74a89962f9e4309eb60968e7d969f29
James Rowe <jnrowe@gmail.com>
2011-06-11 08:00:30 +0100

    Added tests for issue querying.

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -15,3 +15,26 @@ class ReprTests(utils.HttpMockTestCase):
         comments = self.client.issues.comments('ask/python-github2', 24)
         assert_equals(repr(comments[1]),
                       '<Comment: Sure, but I have another idea.\r\n\r\nList methods could return not >')
+
+
+class IssueQueries(utils.HttpMockTestCase):
+    """Test issue querying"""
+    def test_search(self):
+        issues = self.client.issues.search('ask/python-github2', 'timezone')
+        assert_equals(len(issues), 3)
+        assert_equals(issues[2].number, 39)
+
+    def test_list(self):
+        issues = self.client.issues.list('ask/python-github2')
+        assert_equals(len(issues), 5)
+        assert_equals(issues[-1].number, 53)
+
+    def test_list_with_state(self):
+        issues = self.client.issues.list('ask/python-github2', "closed")
+        assert_equals(len(issues), 48)
+        assert_equals(issues[0].number, 52)
+
+    def test_issue_labels(self):
+        labels = self.client.issues.list_labels('JNRowe/misc-overlay')
+        assert_equals(len(labels), 3)
+        assert_equals(labels[0], 'feature')
