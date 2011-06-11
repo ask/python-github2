0a3c36fdcdc3323603b84c979d47602cd8da3c03
James Rowe <jnrowe@gmail.com>
2011-06-11 09:59:47 +0100

    Added issue/comment properties tests.

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,16 +1,49 @@
 import _setup
 
+from datetime import datetime
+
 from nose.tools import assert_equals
 
 import utils
 
 
-class ReprTests(utils.HttpMockTestCase):
+class Issue(utils.HttpMockTestCase):
+    def test_properties(self):
+        issue = self.client.issues.show('ask/python-github2', 24)
+        assert_equals(issue.position, 24.0)
+        assert_equals(issue.number, 24)
+        assert_equals(issue.votes, 0)
+        assert_equals(len(issue.body), 164)
+        assert_equals(issue.title, 'Pagination support for commits.')
+        assert_equals(issue.user, 'svetlyak40wt')
+        assert_equals(issue.state, 'open')
+        assert_equals(issue.labels, [])
+        assert_equals(issue.created_at, datetime(2010, 12, 8, 23, 50, 26))
+        assert_equals(issue.closed_at, None)
+        assert_equals(issue.updated_at, datetime(2011, 1, 4, 16, 26, 7))
+        assert_equals(issue.diff_url,
+                      'https://github.com/ask/python-github2/pull/24.diff')
+        assert_equals(issue.patch_url,
+                      'https://github.com/ask/python-github2/pull/24.patch')
+        assert_equals(issue.pull_request_url,
+                      'https://github.com/ask/python-github2/pull/24')
+
     def test_issue_repr(self):
         issue = self.client.issues.show('ask/python-github2', 24)
         assert_equals(repr(issue),
                       '<Issue: Pagination suppor...>')
-        
+
+
+class Comment(utils.HttpMockTestCase):
+    def test_properties(self):
+        comments = self.client.issues.comments('ask/python-github2', 24)
+        comment = comments[0]
+        assert_equals(comment.created_at, datetime(2010, 12, 9, 22, 37, 26))
+        assert_equals(comment.updated_at, datetime(2010, 12, 9, 22, 37, 26))
+        assert_equals(len(comment.body), 267)
+        assert_equals(comment.id, 601871)
+        assert_equals(comment.user, 'nvie')
+
     def test_comment_repr(self):
         comments = self.client.issues.comments('ask/python-github2', 24)
         assert_equals(repr(comments[1]),
