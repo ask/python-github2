56a1e1429e1db8e6d364da1ac66a78112df4c5db
James Rowe <jnrowe@gmail.com>
2011-06-11 08:03:25 +0100

    Added tests for organisation querying.

--- tests/test_organizations.py
+++ tests/test_organizations.py
@@ -10,3 +10,16 @@ class Organization(utils.HttpMockTestCase):
         organization = self.client.organizations.show('github')
         assert_equals(repr(organization),
                       '<Organization: github>')
+
+
+class OrganizationQueries(utils.HttpMockTestCase):
+    """Test organisation querying"""
+    def test_public_repositories(self):
+        repos = self.client.organizations.public_repositories('github')
+        assert_equals(len(repos), 26)
+        assert_equals(repos[2].name, 'hubahuba')
+
+    def test_public_members(self):
+        members = self.client.organizations.public_members('github')
+        assert_equals(len(members), 33)
+        assert_equals(members[2].name, 'Ben Burkert')
