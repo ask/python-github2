e8728111d42e3823bdadd2c2450f13a1ff98e1fe
James Rowe <jnrowe@gmail.com>
2011-06-11 07:58:28 +0100

    Added test for listing pull requests.

--- tests/test_pull_requests.py
+++ tests/test_pull_requests.py
@@ -10,3 +10,11 @@ class PullRequest(utils.HttpMockTestCase):
         pull_request = self.client.pull_requests.show('ask/python-github2', 39)
         assert_equals(repr(pull_request),
                       '<PullRequest: Datetime timezone handling.>')
+
+
+class PullRequestQueries(utils.HttpMockTestCase):
+    """Test pull request querying"""
+    def test_list(self):
+        pull_requests = self.client.pull_requests.list('ask/python-github2')
+        assert_equals(len(pull_requests), 2)
+        assert_equals(pull_requests[0].title, 'Datetime timezone handling.')
