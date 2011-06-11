6df986ed1e36232fd2d7a31a15dd8c6b04d33b0e
James Rowe <jnrowe@gmail.com>
2011-06-11 09:49:40 +0100

    Added commit properties tests.

--- tests/test_commits.py
+++ tests/test_commits.py
@@ -1,16 +1,39 @@
 import _setup
 
+from datetime import datetime
+
 from nose.tools import assert_equals
 
 import utils
 
 
-class Commit(utils.HttpMockTestCase):
+class CommitProperties(utils.HttpMockTestCase):
+    """Test commit property handling"""
+    commit_id = '1c83cde9b5a7c396a01af1007fb7b88765b9ae45'
+    def test_commit(self):
+        commit = self.client.commits.show('ask/python-github2', self.commit_id)
+        assert_equals(commit.message,
+                      'Added cache support to manage_collaborators.')
+        assert_equals(commit.parents,
+                      [{"id": '7d1c855d2f44a55e4b90b40017be697cf70cb4a0'}])
+        assert_equals(commit.url,
+                      '/ask/python-github2/commit/%s' % self.commit_id)
+        assert_equals(commit.author['login'], 'JNRowe')
+        assert_equals(commit.id, self.commit_id)
+        assert_equals(commit.committed_date,
+                      datetime(2011, 6, 6, 16, 13, 50))
+        assert_equals(commit.authored_date, datetime(2011, 6, 6, 16, 13, 50))
+        assert_equals(commit.tree, 'f48fcc1a0b8ea97f3147dc42cf7cdb6683493e94')
+        assert_equals(commit.committer['login'], 'JNRowe')
+        assert_equals(commit.added, None)
+        assert_equals(commit.removed, None)
+        assert_equals(commit.modified[0]['filename'], 'github2/bin/manage_collaborators.py')
+
+
     def test_repr(self):
-        commit_id = '1c83cde9b5a7c396a01af1007fb7b88765b9ae45'
-        commit = self.client.commits.show('ask/python-github2', commit_id)
+        commit = self.client.commits.show('ask/python-github2', self.commit_id)
         assert_equals(repr(commit),
-                      '<Commit: %s Added cache suppo...>' % commit_id[:8])
+                      '<Commit: %s Added cache suppo...>' % self.commit_id[:8])
 
 
 class CommitsQueries(utils.HttpMockTestCase):
