ask/python-github2

commit 6feaabb94add6e8b002459bace9bc24f428ed0dc

Minor PEP-8 fixes.

github2/core.py
@@ -51,7 +51,7 @@ class GithubCommand(object):
         filter = kwargs.get("filter")
         post_data = kwargs.get("post_data") or {}
         method = kwargs.get("method", "GET")
-        if method.upper() == "POST" or method.upper() == "GET" and post_data:
+        if method.upper() in ("POST", "GET") and post_data:
             response = self.request.post(self.domain, command, *args,
                                          **post_data)
         elif method.upper() == "PUT":

github2/organizations.py
@@ -2,7 +2,6 @@ from github2.core import BaseData, GithubCommand, Attribute, DateAttribute
 from github2.repositories import Repository
 from github2.teams import Team
 from github2.users import User
-import urllib
 
 
 class Organization(BaseData):
@@ -63,13 +62,13 @@ class Organizations(GithubCommand):
 
         :param str organization: organization to list members for
         """
-        return self.get_values(organization, 'public_members',
-                               filter="users", datatype=User)
+        return self.get_values(organization, 'public_members', filter="users",
+                               datatype=User)
 
     def teams(self, organization):
         """Get list of teams in an organization
 
         :param str organization: organization to list teams for
         """
-        return self.get_values(organization, 'teams',
-                               filter="teams", datatype=Team)
+        return self.get_values(organization, 'teams', filter="teams",
+                               datatype=Team)

github2/teams.py
@@ -1,6 +1,7 @@
-from github2.core import BaseData, GithubCommand, Attribute, DateAttribute
+from github2.core import BaseData, GithubCommand, Attribute
 from github2.repositories import Repository
 from github2.users import User
+
 
 class Team(BaseData):
     id = Attribute("The team id")
@@ -9,6 +10,7 @@ class Team(BaseData):
 
     def __repr__(self):
         return "<Team: %s>" % self.name
+
 
 class Teams(GithubCommand):
     domain = "teams"

commit 22adc58c8a8f8f1335709e379d6b186759bad866

Fixed empty POST requests.
Fixes regression introduced in 6feaabb9.

github2/core.py
@@ -134,7 +134,7 @@ class GithubCommand(object):
         filter = kwargs.get("filter")
         post_data = kwargs.get("post_data") or {}
         method = kwargs.get("method", "GET")
-        if method.upper() in ("POST", "GET") and post_data:
+        if method.upper() == "POST" or method.upper() == "GET" and post_data:
             response = self.request.post(self.domain, command, *args,
                                          **post_data)
         elif method.upper() == "PUT":