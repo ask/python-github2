ab46a17eb0e4c6b49fe41aa60b232bd95c869e03
James Rowe <jnrowe@gmail.com>
2011-12-15 20:39:02 +0000

    Activat the Sphinx coverage extension.

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ["sphinx.ext.%s" % ext
-              for ext in ["autodoc", "todo", "intersphinx", "viewcode"]] + \
+              for ext in ["autodoc", "todo", "intersphinx", "viewcode",
+                         "coverage"]] + \
     ["sphinxcontrib.%s" % ext for ext in ["cheeseshop", ]]
 
 # Add any paths that contain templates here, relative to this directory.
