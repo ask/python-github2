ask/python-github2

commit de1f059f7ff5b5a1f202c68441d748dd9e39baab

Use system certificates database, if possible.
Closes #68.

github2/request.py
@@ -1,8 +1,9 @@
 import datetime
 import logging
 import re
+import sys
 import time
-import httplib2
+
 try:
     # For Python 3
     from http.client import responses
@@ -28,12 +29,32 @@ except ImportError:
         from cgi import parse_qs
     from urllib import urlencode, quote
 
+import httplib2
+
 
 #: Hostname for API access
 GITHUB_URL = "https://github.com"
 
 #: Logger for requests module
 LOGGER = logging.getLogger('github2.request')
+
+#: Whether github2 is using the system's certificates for SSL connections
+SYSTEM_CERTS = not httplib2.CA_CERTS.startswith(path.dirname(httplib2.__file__))
+if not SYSTEM_CERTS and sys.platform.startswith('linux'):
+    for cert_file in ['/etc/ssl/certs/ca-certificates.crt',
+                      '/etc/pki/tls/certs/ca-bundle.crt']:
+        if path.exists(cert_file):
+            httplib2.CA_CERTS = cert_file
+            SYSTEM_CERTS = True
+            break
+elif not SYSTEM_CERTS and sys.platform.startswith('freebsd'):
+    if path.exists('/usr/local/share/certs/ca-root-nss.crt'):
+        httplib2.CA_CERTS = '/usr/local/share/certs/ca-root-nss.crt'
+        SYSTEM_CERTS = True
+if SYSTEM_CERTS:
+    LOGGER.info('Using system certificates in %r', httplib2.CA_CERTS)
+else:
+    LOGGER.warning('Using bundled certificates for HTTPS connections')
 
 
 def charset_from_headers(headers):
@@ -105,12 +126,11 @@ class GithubRequest(object):
         digicert_ha_cert = path.join(path.dirname(path.abspath(__file__)),
                                      "DigiCert_High_Assurance_EV_Root_CA.crt")
         if proxy_host is None:
-            self._http = httplib2.Http(cache=cache, ca_certs=digicert_ha_cert)
+            self._http = httplib2.Http(cache=cache)
         else:
             proxy_info = httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP,
                                             proxy_host, proxy_port)
-            self._http = httplib2.Http(proxy_info=proxy_info, cache=cache,
-                                       ca_certs=digicert_ha_cert)
+            self._http = httplib2.Http(proxy_info=proxy_info, cache=cache)
 
     def encode_authentication_data(self, extra_post_data):
         post_data = {}

tests/utils.py
@@ -25,7 +25,7 @@ class HttpMock(object):
     Implementation tests should never span network boundaries
     """
 
-    def __init__(self, cache=None, timeout=None, proxy_info=None, ca_certs=None):
+    def __init__(self, cache=None, timeout=None, proxy_info=None):
         """Create a mock httplib.Http object
 
         .. attribute: called_with

commit d32273af8b67a671bab242c9808a309f07398108

Fix for users unable to use system certificates.
Regression that snuck in de1f059f7ff5b5a1f202c68441d748dd9e39baab.

github2/request.py
@@ -123,14 +123,15 @@ class GithubRequest(object):
                 "api_version": self.api_version,
                 "api_format": self.api_format,
             }
-        digicert_ha_cert = path.join(path.dirname(path.abspath(__file__)),
-                                     "DigiCert_High_Assurance_EV_Root_CA.crt")
         if proxy_host is None:
             self._http = httplib2.Http(cache=cache)
         else:
             proxy_info = httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP,
                                             proxy_host, proxy_port)
             self._http = httplib2.Http(proxy_info=proxy_info, cache=cache)
+        if not SYSTEM_CERTS:
+            self._http.ca_certs = path.join(path.dirname(path.abspath(__file__)),
+                                            "DigiCert_High_Assurance_EV_Root_CA.crt")
 
     def encode_authentication_data(self, extra_post_data):
         post_data = []