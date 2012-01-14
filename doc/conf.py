6a4380053566e56d1dfd352a1f6de72f13ba17e5
James Rowe <jnrowe@gmail.com>
2012-01-14 12:35:58 +0000

    Bump copyright years.

--- doc/conf.py
+++ doc/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'github2'
-copyright = u'2011, Ask Solem'
+copyright = u'2009-2012, Ask Solem'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
