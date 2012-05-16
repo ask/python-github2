6035cc3784543650fa8c9a731908c1b49d327cb4
James Rowe <jnrowe@gmail.com>
2012-05-16 19:45:03 +0100

    Http mocking switched to use mock.Mock object.

--- tests/utils.py
+++ tests/utils.py
@@ -11,6 +11,8 @@ from email import message_from_file
 
 import httplib2
 
+from mock import Mock
+
 from github2.client import Github
 from github2.request import charset_from_headers
 
@@ -19,32 +21,26 @@ if sys.version_info[0] == 2:
     bytes = lambda x, enc: x
 
 
-HTTP_DATA_DIR = "tests/data/"
-
-ORIG_HTTP_OBJECT = httplib2.Http
+ORIG_REQUEST_METHOD = httplib2.Http.request
 
 
-class HttpMock(object):
-    """Simple Http mock that returns saved entries.
+def request_mock(uri, method='GET', body=None, headers=None,
+              redirections=5, connection_type=None):
+    """Http mock side effect that returns saved entries.
 
     Implementation tests should never span network boundaries.
 
     """
 
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def request(self, uri, method='GET', body=None, headers=None,
-                redirections=5, connection_type=None):
-        file = os.path.join(HTTP_DATA_DIR, httplib2.safename(uri))
-        if os.path.exists(file):
-            response = message_from_file(open(file))
-            headers = httplib2.Response(response)
-            body = bytes(response.get_payload(), charset_from_headers(headers))
-            return (headers, body)
-        else:
-            return (httplib2.Response({"status": "404"}),
-                    "Resource %r unavailable from test data store" % file)
+    file = os.path.join("tests/data", httplib2.safename(uri))
+    if os.path.exists(file):
+        response = message_from_file(open(file))
+        headers = httplib2.Response(response)
+        body = bytes(response.get_payload(), charset_from_headers(headers))
+        return (headers, body)
+    else:
+        return (httplib2.Response({"status": "404"}),
+                "Resource %r unavailable from test data store" % file)
 
 
 class HttpMockTestCase(unittest.TestCase):
@@ -58,7 +54,8 @@ class HttpMockTestCase(unittest.TestCase):
         in tests.
 
         """
-        httplib2.Http = HttpMock
+        httplib2.Http.request = Mock(spec_set=httplib2.Http.request,
+                                     side_effect=request_mock)
         self.client = Github()
 
     def tearDown(self):
@@ -67,7 +64,7 @@ class HttpMockTestCase(unittest.TestCase):
         `httplib2.Http` is returned to its original state.
 
         """
-        httplib2.Http = ORIG_HTTP_OBJECT
+        httplib2.Http.request = ORIG_REQUEST_METHOD
 
 
 class HttpMockAuthenticatedTestCase(HttpMockTestCase):
@@ -80,21 +77,6 @@ class HttpMockAuthenticatedTestCase(HttpMockTestCase):
         in tests.
 
         """
-        httplib2.Http = HttpMock
+        httplib2.Http.request = Mock(spec_set=httplib2.Http.request,
+                                     side_effect=request_mock)
         self.client = Github(access_token='xxx')
-
-
-def set_http_mock():
-    """Function to enable ``Http`` mock
-
-    This is useful in simple `nose`-compliant test functions
-    """
-    httplib2.Http = HttpMock
-
-
-def unset_http_mock():
-    """Function to disable ``Http`` mock
-
-    :see: :func:`set_http_mock`
-    """
-    httplib2.Http = ORIG_HTTP_OBJECT
